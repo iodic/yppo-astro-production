1bdb0fa177fdcd0e2b03e96233fdfd7002137255
Marko Ranđelović <marko@stuntcoders.com>
2024-03-07 11:29:04 +0100

    Adjust video responsive styles

--- src/layouts/shortcodes/Video.jsx
+++ src/layouts/shortcodes/Video.jsx
@@ -1,7 +1,7 @@
-const Video = ({ title, width = 500, height = "auto", src, ...rest }) => {
+const Video = ({ title, width = 600, height = "auto", src, ...rest }) => {
   return (
     <video
-      className="overflow-hidden rounded-md shadow-2xl"
+      className="overflow-hidden rounded-md shadow-2xl w-full object-contain mx-auto max-w-3xl w-[94%]"
       width={width}
       height={height}
       controls
