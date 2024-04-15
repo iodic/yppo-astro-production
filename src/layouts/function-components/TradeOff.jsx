1db93ee1b71cd753ac732c216c76ac0f5544651b
Nikola Pavlovic <nikola@stuntcoders.com>
2024-04-15 11:43:37 +0200

    Add tradeoffs component

---
+++ src/layouts/function-components/TradeOff.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import BlockContent from "@sanity/block-content-to-react";
+
+const TradeOff = ({ content, type }) => {
+  const consClasses =
+    "card-cons rounded-l bg-white mb-6 px-6 pb-6 shadow-lg border-left border-l-4 border-l-[#8ebc0c]";
+  const prosClasses =
+    "card-pros rounded-l bg-white mb-6 px-6 pb-6 shadow-lg border-left border-l-4 border-l-[#f3873c]";
+
+  return (
+    <div className={type === "CONS" ? consClasses : prosClasses}>
+      <p className="pt-4 pl-2 uppercase text-xl font-semibold text-black">
+        {type}
+      </p>
+      <BlockContent blocks={content} />
+    </div>
+  );
+};
+
+export default TradeOff;
