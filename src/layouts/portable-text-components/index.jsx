ad01008d936d8e5954feb2a5f9034ed7810fdbe3
Miljana Rancic <miljana@stuntcoders.com>
2024-04-23 09:24:07 +0200

    Import new components to index.jsx

---
+++ src/layouts/portable-text-components/index.jsx
@@ -0,0 +1,9 @@
+import BlockComponents from "./BlockComponents";
+import ListComponents from "./ListComponents";
+import ListItemComponents from "./ListItemComponents";
+
+export default {
+  block: BlockComponents,
+  list: ListComponents,
+  listItem: ListItemComponents,
+};
