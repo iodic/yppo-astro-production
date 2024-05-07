089e133a0d9f2bab9c8e4a23b14df1c1120b0ac7
Nikola Pavlovic <nikola@stuntcoders.com>
2024-05-07 13:51:56 +0200

    Change to use promise

--- api/postmark.ts
+++ api/postmark.ts
@@ -1,30 +1,31 @@
 import postmark from "postmark";
 
-export default async function handler(req: any, res: any) {
-  try {
-    const { body } = req;
+export default function handler(req:any, res:any) {
+  const { body } = req;
 
-    const client = new postmark.ServerClient(
-      "20117eea-da45-43d0-a313-b55475dee001",
-    );
+  const client = new postmark.ServerClient(
+    "20117eea-da45-43d0-a313-b55475dee001",
+  );
 
-    const message = {
-      From: "marko@stuntcoders.com",
-      To: "nikola@stuntcoders.com",
-      Subject: "New Contact Form Submission",
-      TextBody: `
-        Name: ${body.name}
-        Email: ${body.email}
-        Reason: ${body.reason}
-        Message: ${body.message}
-      `,
-    };
+  const message = {
+    From: "marko@stuntcoders.com",
+    To: "nikola@stuntcoders.com",
+    Subject: "New Contact Form Submission",
+    TextBody: `
+      Name: ${body.name}
+      Email: ${body.email}
+      Reason: ${body.reason}
+      Message: ${body.message}
+    `,
+  };
 
-    await client.sendEmail(message);
-
-    return new Response(`Email sent successfully`);
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return new Response(`Error sending email`);
-  }
+  client
+    .sendEmail(message)
+    .then(() => {
+      res.send(`Email sent successfully`);
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+      res.send(`Error sending email`);
+    });
 }
