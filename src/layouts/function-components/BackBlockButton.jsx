176f5fa20c6dc15cb4d544a9afb734a0689a6202
Miljana Rancic <miljana@stuntcoders.com>
2024-05-21 12:18:02 +0200

    Add BackBlockButton component

---
+++ src/layouts/function-components/BackBlockButton.jsx
@@ -0,0 +1,12 @@
+export const BackBlockButton = ({ text, arrow, onClick }) => (
+  <button
+    onClick={onClick}
+    className={`flex items-center jump-back py-1 px-3 w-full text-left transition-all mb-6 bg-sky-100 ${arrow ? "hover:pl-2" : "justify-between"}`}
+  >
+    {arrow && (
+      <img src="/images/icons/back-arrow.svg" className="w-5 mt-1 mr-1"></img>
+    )}
+    {text}
+    {!arrow && <img src="/images/icons/close.svg" className="w-3"></img>}
+  </button>
+);
