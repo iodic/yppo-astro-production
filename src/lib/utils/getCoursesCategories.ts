e8200962f2a94864afc3fd35d11fd25a2074849e
Jovana Ranđelović <jovana@stuntcoders.com>
2024-05-22 13:24:28 +0200

    Add get courses categories util

---
+++ src/lib/utils/getCoursesCategories.ts
@@ -0,0 +1,20 @@
+const getCoursesCategories = (posts: []) => {
+  let postTagsArrays = posts.map((post: any) => post.tags);
+  let allCategories: object[] = [];
+
+  postTagsArrays.map((tagArray: any[]) => {
+    tagArray.map((tagItem) => {
+      const uniqueObject = allCategories.some((object) =>
+        Object.keys(object).includes(tagItem.tagSlug?.current),
+      );
+
+      if (!uniqueObject) {
+        allCategories.push({ [`${tagItem.tagSlug?.current}`]: tagItem?.tag });
+      }
+    });
+  });
+
+  return allCategories;
+};
+
+export default getCoursesCategories;
