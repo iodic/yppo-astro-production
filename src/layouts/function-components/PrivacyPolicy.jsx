a1a8da1c9bd0c66b2fb6e8f7c24e7bf34417bfd1
Marija Cehic <marijac@stuntcoders.com>
2024-06-27 16:25:37 +0200

    Add Privacy Policy page templates

---
+++ src/layouts/function-components/PrivacyPolicy.jsx
@@ -0,0 +1,10 @@
+import { PortableText } from "@portabletext/react";
+import portableTextComponents from "../portable-text-components";
+
+const PrivacyPolicyPortableText = ({ content }) => {
+  return (
+    <PortableText value={content} components={portableTextComponents} />
+  );
+};
+
+export default PrivacyPolicyPortableText;
