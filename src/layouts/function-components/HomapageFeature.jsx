3150aca181a1e8f048bca30d22830c7bc471bcdc
Miljana Rancic <miljana@stuntcoders.com>
2024-06-28 17:34:37 +0200

    Update Homepage cards logic & style

--- src/layouts/function-components/HomapageFeature.jsx
+++ src/layouts/function-components/HomapageFeature.jsx
@@ -1,70 +1,31 @@
-import { humanize } from "@/lib/utils/textConverter";
-import * as Icon from "react-feather";
+import { PortableText } from "@portabletext/react";
+import portableTextComponents from "../portable-text-components";
 
 const HomapageFeature = ({ cards }) => {
-  const isTouchScreenDevice = () => {
-    try {
-      document.createEvent("TouchEvent");
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const onCardClick = (e) => {
-    const cardWrapper = e.target.closest(".flip-card-inner");
-
-    if (!isTouchScreenDevice()) {
-      cardWrapper.classList.add("flip-card-inner-back");
-    } else {
-      if (cardWrapper.classList.contains("flipped")) {
-        cardWrapper.classList.toggle("flip-card-inner-back");
-        cardWrapper.classList.remove("flipped");
-      }
-
-      if (!cardWrapper.classList.contains("flipped")) {
-        cardWrapper.classList.add("flipped");
-      }
-    }
-  };
-
-  const onHoverLeave = (e) => {
-    if (!isTouchScreenDevice()) {
-      const cardWrapper = e.target.closest(".flip-card-inner");
-      cardWrapper.classList.remove("flip-card-inner-back");
-    }
-  };
-
   return (
-    <div className="key-feature-grid mt-10 grid gap-7 grid-cols-1 md:grid-cols-8">
-      {cards.map((item, i) => {
-        const { title, description, shortDescription, icon } = item;
-        const FeatherIcon = Icon[humanize(icon)];
+    <div className="flex flex-col mt-5">
+      {cards.map((item, index) => {
+        const { title, description, cardImage } = item;
 
         return (
-          <div
-            key={i}
-            className={`flex flex-col justify-between ${i === 2 ? "md:col-span-4 md:col-start-3" : "md:col-span-4"}`}
-          >
-            <div
-              className="flip-card"
-              onMouseLeave={(e) => onHoverLeave(e)}
-              onClick={(e) => onCardClick(e)}
-            >
-              <div className="flip-card-inner">
-                <div className="flip-card-front rounded-lg bg-white p-5 shadow-lg flex flex-col justify-between">
-                  <div>
-                    <h3 className="h4 text-xl lg:text-2xl">{title}</h3>
-                    <p>{shortDescription}</p>
-                  </div>
-                  <span className="icon mt-4">
-                    <FeatherIcon />
-                  </span>
-                </div>
+          <div key={index} className="sm:mt-8">
+            <div className="py-3 my-3 sm:py-5 lg:my-5">
+              <h2 className="text-3xl sm:text-4xl text-center sm:text-left mb-5">
+                {title}
+              </h2>
+              <img
+                class={`object-contain mb-3 sm:mb-0 sm:w-[48%] md:w-[40%] ${index === 0 ? "sm:float-left sm:mr-6" : "sm:float-right sm:ml-6"}`}
+                alt="card-image"
+                src={cardImage}
+              />
 
-                <div className="flip-card-back rounded-lg bg-white p-5 shadow-lg flex items-center text-black">
-                  <p>{description}</p>
-                </div>
+              <div
+                className={`w-[90%] sm:w-auto mx-auto sm:mx-0 ${index === 0 ? "sm:mt-12" : ""}`}
+              >
+                <PortableText
+                  value={description}
+                  components={portableTextComponents}
+                />
               </div>
             </div>
           </div>
