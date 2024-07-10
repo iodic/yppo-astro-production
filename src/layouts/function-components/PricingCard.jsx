e0feef6d55467c4bcade4aa439b9b06775db25a9
Miljana Rancic <miljana@stuntcoders.com>
2024-07-10 16:40:49 +0200

    Update PricingCard buttons

--- src/layouts/function-components/PricingCard.jsx
+++ src/layouts/function-components/PricingCard.jsx
@@ -3,7 +3,7 @@ import { humanize } from "@/lib/utils/textConverter";
 
 import * as Icon from "react-feather";
 
-const PricingCard = ({ card, generalText }) => {
+const PricingCard = ({ card, cardAction, generalText }) => {
   const {
     title,
     description,
@@ -14,7 +14,8 @@ const PricingCard = ({ card, generalText }) => {
     pricePerEmployee,
   } = card;
 
-  const { numberOfEmployeesText, mainButtonText, ctaText } = generalText;
+  const { urlTitle, url } = cardAction || {};
+  const { numberOfEmployeesText } = generalText;
 
   const CardIcon = Icon[humanize(cardIcon)];
 
@@ -145,29 +146,12 @@ const PricingCard = ({ card, generalText }) => {
         <div className="text-center">
           <a
             className={`btn ${
-              featured ? "btn-primary" : "btn-outline-white"
+              featured ? "btn-primary capitalize" : "btn-outline-white"
             } block h-[48px] w-full rounded-[50px] leading-[30px]`}
-            href="#"
+            href={url}
+            target="_blank"
           >
-            {mainButtonText ? mainButtonText : "Buy Now"}
-          </a>
-
-          <a className="mt-6 inline-flex items-center text-dark" href="#">
-            {ctaText ? ctaText : "Request a Demo"}
-
-            <svg
-              className="ml-1.5"
-              width="13"
-              height="16"
-              viewBox="0 0 13 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12.7071 8.70711C13.0976 8.31658 13.0976 7.68342 12.7071 7.29289L6.34315 0.928932C5.95262 0.538408 5.31946 0.538408 4.92893 0.928932C4.53841 1.31946 4.53841 1.95262 4.92893 2.34315L10.5858 8L4.92893 13.6569C4.53841 14.0474 4.53841 14.6805 4.92893 15.0711C5.31946 15.4616 5.95262 15.4616 6.34315 15.0711L12.7071 8.70711ZM0 9H12V7H0V9Z"
-                fill="currentColor"
-              ></path>
-            </svg>
+            {urlTitle ? urlTitle : "Schedule A Demo"}
           </a>
         </div>
       </div>
