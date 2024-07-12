491c503314bc3aa2380ae394cda12cca6dccce42
Jovana Ranđelović <jovana@stuntcoders.com>
2024-07-12 13:30:20 +0200

    Remove unnecessary animations/commented out code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-const plugin = require("tailwindcss/plugin");
 const theme = require("./src/config/theme.json");
 
 let font_base = Number(theme.fonts.font_size.base.replace("px", ""));
@@ -70,23 +69,6 @@ module.exports = {
         secondary: ["var(--font-secondary)", fontSecondaryType],
       },
       keyframes: {
-        typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden",
-          },
-          "100%": {
-            width: "100%",
-          },
-        },
-        blink: {
-          "50%": {
-            borderColor: "transparent",
-          },
-          "100%": {
-            borderColor: "white",
-          },
-        },
         "letter-color": {
           0: {
             color: "transparent",
@@ -97,9 +79,6 @@ module.exports = {
         },
       },
       animation: {
-        typing: "typing 6s steps(10) infinite alternate, blink .7s infinite",
-        "typing-short": "typing 3s steps(11), blink .7s infinite",
-        "typing-long": "typing 4s steps(24), blink .7s infinite",
         "letter-color": "letter-color 1s normal forwards",
       },
     },
@@ -117,20 +96,6 @@ module.exports = {
         4: "1.5rem",
         5: "3rem",
       },
-    }),
-    plugin(({ matchUtilities, theme }) => {
-      matchUtilities(
-        {
-          "animation-delay": (value) => {
-            return {
-              "animation-delay": value,
-            };
-          },
-        },
-        {
-          values: theme("transitionDelay"),
-        },
-      );
     }),
   ],
 };
